d816444e9d734bbb57ad004a58afa4485254096f
Yurii-Bodnar <yuriy.bodnar75@gmail.com>
2022-10-05 16:53:14 +0300

    delate

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,36 +1,15 @@
-import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-  } from 'redux-persist'
-import storage from 'redux-persist/lib/storage';
+
 import { contactsReduser } from './contanctsSlice';
 import { configureStore } from '@reduxjs/toolkit';
 import { filterReduser } from './filterSlice';
 
-const persistConfig = {
-  key: 'contacts',
-//   version:1,
-  storage,
-};
-const persistContactsReducer = persistReducer(persistConfig, contactsReduser);
 
 const store = configureStore({
   reducer: {
-    contacts: persistContactsReducer,
+    contacts: contactsReduser,
     filter: filterReduser,
   },
-  middleware: (getDefaultMiddleware) =>
-  getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
+
 });
-export const persistor = persistStore(store)
+
 export default store;
